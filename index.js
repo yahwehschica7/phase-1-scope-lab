c72ea7ebd56689c94f7190a845e6fa4bf3b3820c
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-29 13:15:23 -0400

    commit tree a6431721db2ac139fef1fd9ad1a1765090466d0d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+ function upperCaseCustomerName() {
+     customerName = 'BOB'
+ }
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+   }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'Sal'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Mary';
+}
+
